46b14187c82a798ba08aba76c96c12dadc6b798e
Santiago Gonzalez <hernangt12re3@gmail.com>
2023-02-09 17:36:45 -0500

    Module 2 exercise

--- client/index.js
+++ client/index.js
@@ -4,14 +4,28 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const merkleTree = new MerkleTree(niceList);
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const randomClaimerIndex = Math.round(Math.random() * niceList.length);
+  const name = niceList[randomClaimerIndex];
+  const proof = merkleTree.getProof(randomClaimerIndex);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    name,
   });
 
-  console.log({ gift });
+  console.log(`Proving for ${name}`, { gift });
+
+  const fakeClaimer = 'Adam Back';
+  const fakeIndex = Math.round(Math.random() * niceList.length);
+  const fakeProof = merkleTree.getProof(fakeIndex);
+  const { data: rs } = await axios.post(`${serverUrl}/gift`, {
+    proof: fakeProof,
+    name: fakeClaimer,
+  });
+  console.log(`Proving for ${fakeClaimer}`, { rs });
 }
 
 main();
--- utils/example.js
+++ utils/example.js
@@ -2,11 +2,14 @@ const MerkleTree = require('./MerkleTree');
 const niceList = require('./niceList');
 const verifyProof = require('./verifyProof');
 
+console.log('Nicelist', niceList.length);
 // create the merkle tree for the whole nice list
 const merkleTree = new MerkleTree(niceList);
 
 // get the root
 const root = merkleTree.getRoot();
+
+console.log('Merkle Root', root);
 
 // find the proof that norman block is in the list 
 const name = 'Norman Block';
@@ -17,3 +20,12 @@ const proof = merkleTree.getProof(index);
 console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+const fakeClaimer = 'Adam Back';
+const fakeIndex = Math.round(Math.random() * niceList.length);
+const fakeProof = merkleTree.getProof(fakeIndex);
+console.log(`Fake Proof for ${fakeClaimer}?`, !verifyProof(fakeProof, fakeClaimer, root)); // Fake proof
+
+const randomClaimerIndex = Math.round(Math.random() * niceList.length);
+const randomClaimer = niceList[randomClaimerIndex];
+const randomProof = merkleTree.getProof(randomClaimerIndex);
+console.log(`Proof for ${randomClaimer}?`, verifyProof(randomProof, randomClaimer, root)); // Good proof
